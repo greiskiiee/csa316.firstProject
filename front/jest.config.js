a73d484ec60240ffae99f969594f2d453fd0caef
chimegerdene <chimgeebatuh@gmail.com>
2026-04-01 08:23:43 +0800

    fix: use next/jest config to support both tests and Vercel build

--- front/jest.config.js
+++ front/jest.config.js
@@ -1,13 +1,13 @@
-module.exports = {
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({ dir: "./" });
+
+const customJestConfig = {
   testEnvironment: "jsdom",
-
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
-  },
-
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
-
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 };
+
+module.exports = createJestConfig(customJestConfig);
